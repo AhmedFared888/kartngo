6318189691e99f93ea84104f20122a708b11c2ff
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-30 21:57:54 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const kartngo());
+}
+
+class kartngo extends StatelessWidget {
+  const kartngo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
