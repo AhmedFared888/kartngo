1cf88d75c8a8cc62d706974871918acd0d4bad36
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-08-02 16:02:01 -0700

    adding increase and decrease buttons

--- lib/features/home/presentation/views/widgets/count_box.dart
+++ lib/features/home/presentation/views/widgets/count_box.dart
@@ -3,38 +3,56 @@ import 'package:kartngo/core/utils/color_manager.dart';
 import 'package:kartngo/core/utils/styles_manager.dart';
 import 'package:kartngo/core/utils/values_manager.dart';
 
-class CountBox extends StatelessWidget {
+class CountBox extends StatefulWidget {
   const CountBox({
     super.key,
   });
 
+  @override
+  State<CountBox> createState() => _CountBoxState();
+}
+
+class _CountBoxState extends State<CountBox> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         color: ColorManager.backGroundColor,
         borderRadius: BorderRadius.circular(
-          AppSize.s10,
+          AppSize.s20,
         ),
       ),
       child: Row(
         children: [
           IconButton(
-            onPressed: () {},
+            onPressed: decrease,
             icon: const Icon(Icons.remove),
           ),
           Text(
-            '0',
+            '$counter',
             style: StylesManager.textStyle20(
               ColorManager.primaryColor,
             ),
           ),
           IconButton(
-            onPressed: () {},
+            onPressed: increase,
             icon: const Icon(Icons.add),
           ),
         ],
       ),
     );
+  }
+
+  void increase() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  void decrease() {
+    setState(() {
+      counter--;
+    });
   }
 }
