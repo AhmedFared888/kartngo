3a9eff00481af3a4ce6332d7c07d3a5c5d8b670a
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-08-02 14:07:20 -0700

    create color manager class

---
+++ lib/core/utils/color_manager.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color primaryColor = Color(0xff0e374e);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color black = Color(0xff000000);
+  static const Color error = Color(0xffe61f34);
+  static const Color lightblue = Color(0xff578bb0);
+  static const Color backGroundColor = Color(0xfff3f8fc);
+}
