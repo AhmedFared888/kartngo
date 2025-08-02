6df250c3f40f8799ee673fd7fe9b5549965099d5
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-08-02 15:58:00 -0700

    fix image issue

--- lib/core/utils/assets_manager.dart
+++ lib/core/utils/assets_manager.dart
@@ -2,5 +2,5 @@ const String imagePath = 'assets/images';
 
 abstract class AssetsManager {
   //home asstes
-  static const testImage = '$imagePath/test.png';
+  static const testImage = '$imagePath/test2.png';
 }
